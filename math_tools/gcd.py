f49751e1ab947eb39287f7f19af797e3e36a9b8e
lassenextkbh <lass365f@edu.next.kbh.dk>
2025-10-08 15:10:07 +0200

    Updated gcd to handle edge cases

--- math_tools/gcd.py
+++ math_tools/gcd.py
@@ -7,16 +7,23 @@ def gcd(a, b):
         b: An integer.
 
     Returns:
-        The greatest common divisor of a and b.
+        The greatest common divisor of a and b, None if the gcd is undefined.
     """
     if not isinstance(a, int) or not isinstance(b, int): # Throw an error if a and b are not integers
         raise Exception("The variables must be integers!")
     
+    if a == 0 and b == 0: # Handle edge cases
+        return None
+    elif a==0:
+        return abs(b)
+    elif b==0:
+        return abs(a)
+    
     if a<0:
-        a = a * -1 # Make sure a = |a|
+        a = abs(a) # Make sure a = |a|
     
     if b<0:
-        b = b * -1 # Make sure b = |b|
+        b = abs(b) # Make sure b = |b|
 
     while a != b: # Loop until a and b are equal
         if b-a>0: # If a can be subtracted from b without b becoming negative
